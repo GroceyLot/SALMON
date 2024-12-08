feb234f2fb862c93a956002c4a1f194ef30fc529
GroceyLot <waltermorton@icloud.com>
2024-12-08 08:20:21 +0000

    Modify some stuff

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -4,25 +4,20 @@ public class UIScript : MonoBehaviour
 {
     private ProgressBar leftBar;
     private ProgressBar rightBar;
-    private ProgressBar middleBar;
     public float leftValue = 0;
     public float rightValue = 0;
-    public float middleValue = 0;
     void OnEnable()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
         leftBar = root.Q<ProgressBar>("LeftBar");
         rightBar = root.Q<ProgressBar>("RightBar");
-        middleBar = root.Q<ProgressBar>("MiddleBar");
     }
 
     void Update()
     {
         leftBar.value = leftValue;
         rightBar.value = rightValue;
-        middleBar.value = middleValue;
-        leftBar.style.display = leftValue == 0 ? DisplayStyle.None : DisplayStyle.Flex;
-        rightBar.style.display = rightValue == 0 ? DisplayStyle.None : DisplayStyle.Flex;
-        middleBar.style.display = middleValue < 0.01f ? DisplayStyle.None : DisplayStyle.Flex;
+        leftBar.style.display = leftValue < 0.01f ? DisplayStyle.None : DisplayStyle.Flex;
+        rightBar.style.display = rightValue < 0.01f ? DisplayStyle.None : DisplayStyle.Flex;
     }
 }
